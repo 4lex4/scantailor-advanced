b21a42b6e4ef415a2bfe6be33c1f911d9202656e
4lex4 <4lex49@zoho.com>
2017-11-03 23:03:52 +0300

    ~ version 1.0.5

--- version.h
+++ version.h
@@ -19,7 +19,7 @@
 #ifndef SCANTAILOR_VERSION_H_
 #define SCANTAILOR_VERSION_H_
 
-#define VERSION "1.0.4 PR"
+#define VERSION "1.0.5 PR"
 #define VERSION_QUAD "" // Must be "x.x.x.x" or an empty string.
 
 #endif

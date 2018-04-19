5c157422eb7c31fb18edd4f2cb97abc398f20e1b
Alex <4lex49@zoho.com>
2018-04-19 14:30:02 +0300

    ~ CMake code reformat.

--- cmake/CopyToBuildDir.cmake
+++ cmake/CopyToBuildDir.cmake
@@ -1,74 +1,74 @@
 # Reset variables.
-FOREACH (conf_ Debug Release MinSizeRel RelWithDebInfo)
-    SET(
+foreach (conf_ Debug Release MinSizeRel RelWithDebInfo)
+    set(
             "COPY_TO_BUILD_DIR_${conf_}" "" CACHE INTERNAL
             "Files to copy to ${conf_} build directory" FORCE
     )
-ENDFOREACH ()
+endforeach()
 
 # Usage:
-# COPY_TO_BUILD_DIR(one or more files [SUBDIR subdir] [CONFIGURATIONS] conf1 conf2 ...)
-MACRO(COPY_TO_BUILD_DIR)
-    SET(files_ "")
-    SET(confs_ "Debug;Release;MinSizeRel;RelWithDebInfo")
-    SET(subdir_ "")
-    SET(out_list_ "files_")
-    FOREACH (arg_ ${ARGV})
-        IF ("${arg_}" STREQUAL "SUBDIR")
-            SET(out_list_ "subdir_")
-        ELSEIF ("${arg_}" STREQUAL "CONFIGURATIONS")
-            SET(out_list_ "confs_")
-            SET(confs_ "")
-        ELSE ()
-            LIST(APPEND ${out_list_} "${arg_}")
-        ENDIF ()
-    ENDFOREACH ()
+# copy_to_build_dir(one or more files [SUBDIR subdir] [CONFIGURATIONS] conf1 conf2 ...)
+macro (copy_to_build_dir)
+    set(files_ "")
+    set(confs_ "Debug;Release;MinSizeRel;RelWithDebInfo")
+    set(subdir_ "")
+    set(out_list_ "files_")
+    foreach (arg_ ${ARGV})
+        if ("${arg_}" STREQUAL "SUBDIR")
+            set(out_list_ "subdir_")
+        elseif ("${arg_}" STREQUAL "CONFIGURATIONS")
+            set(out_list_ "confs_")
+            set(confs_ "")
+        else()
+            list(APPEND ${out_list_} "${arg_}")
+        endif()
+    endforeach()
 
-    LIST(LENGTH subdir_ num_subdirs_)
-    IF ("${num_subdirs_}" GREATER 1)
-        MESSAGE(FATAL_ERROR "Multiple sub-directories aren't allowed!")
-    ENDIF ()
+    list(LENGTH subdir_ num_subdirs_)
+    if ("${num_subdirs_}" GREATER 1)
+        message(FATAL_ERROR "Multiple sub-directories aren't allowed!")
+    endif()
 
-    FOREACH (conf_ ${confs_})
-        FOREACH (file_ ${files_})
-            IF (EXISTS "${file_}")
-                IF ("${subdir_}" STREQUAL "")
-                    LIST(APPEND "COPY_TO_BUILD_DIR_${conf_}" "${file_}")
-                ELSE ()
-                    LIST(APPEND "COPY_TO_BUILD_DIR_${conf_}" "${file_}=>${subdir_}")
-                ENDIF ()
-            ENDIF ()
-        ENDFOREACH ()
+    foreach (conf_ ${confs_})
+        foreach (file_ ${files_})
+            if (EXISTS "${file_}")
+                if ("${subdir_}" STREQUAL "")
+                    list(APPEND "COPY_TO_BUILD_DIR_${conf_}" "${file_}")
+                else()
+                    list(APPEND "COPY_TO_BUILD_DIR_${conf_}" "${file_}=>${subdir_}")
+                endif()
+            endif()
+        endforeach()
 
         # Force the new value to be written to the cache.
-        SET(
+        set(
                 "COPY_TO_BUILD_DIR_${conf_}" ${COPY_TO_BUILD_DIR_${conf_}}
                 CACHE INTERNAL "Files to copy to ${conf_} build directory" FORCE
         )
-    ENDFOREACH ()
-ENDMACRO()
+    endforeach()
+endmacro()
 
 
-MACRO(GENERATE_COPY_TO_BUILD_DIR_TARGET target_name_)
-    SET(script_ "${CMAKE_BINARY_DIR}/copy_to_build_dir.cmake")
-    CONFIGURE_FILE("cmake/copy_to_build_dir.cmake.in" "${script_}" @ONLY)
+macro (generate_copy_to_build_dir_target target_name_)
+    set(script_ "${CMAKE_BINARY_DIR}/copy_to_build_dir.cmake")
+    configure_file("cmake/copy_to_build_dir.cmake.in" "${script_}" @ONLY)
 
-    SET(
+    set(
             src_files_
             ${COPY_TO_BUILD_DIR_Debug} ${COPY_TO_BUILD_DIR_Release}
             ${COPY_TO_BUILD_DIR_MinSizeRel} ${COPY_TO_BUILD_DIR_RelWithDebInfo}
     )
-    SET(deps_ "")
-    FOREACH (src_file_ ${src_files_})
-        STRING(REGEX REPLACE "(.*)=>.*" "\\1" src_file_ "${src_file_}")
-        LIST(APPEND deps_ "${src_file_}")
-    ENDFOREACH ()
+    set(deps_ "")
+    foreach (src_file_ ${src_files_})
+        string(REGEX REPLACE "(.*)=>.*" "\\1" src_file_ "${src_file_}")
+        list(APPEND deps_ "${src_file_}")
+    endforeach()
 
     # Copy DLLs and other stuff to ${CMAKE_BINARY_DIR}/<configuration>
-    ADD_CUSTOM_TARGET(
+    add_custom_target(
             "${target_name_}" ALL
             COMMAND "${CMAKE_COMMAND}" "-DTARGET_DIR=$<TARGET_FILE_DIR:scantailor>"
             "-DCFG=$<CONFIG>" -P "${script_}"
             DEPENDS "${script_}" ${deps_}
     )
-ENDMACRO()
+endmacro()

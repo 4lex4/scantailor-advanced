4lex4/scantailor-advanced

commit 87af48413b4a2357d36f685f2b7334d29d3b7848

Add option to use black as filling background color

src/core/filters/output/ColorCommonOptions.cpp
@@ -37,10 +37,13 @@ bool ColorCommonOptions::operator!=(const ColorCommonOptions& other) const {
 }
 
 FillingColor ColorCommonOptions::parseFillingColor(const QString& str) {
-  if (str == "white") {
-    return FILL_WHITE;
-  } else {
-    return FILL_BACKGROUND;
+  switch(str) {
+	case "white":
+      return FILL_WHITE;
+    case "black":
+      return FILL_BLACK;
+    default:
+      return FILL_BACKGROUND;
   }
 }
 
@@ -49,6 +52,9 @@ QString ColorCommonOptions::formatFillingColor(FillingColor type) {
   switch (type) {
     case FILL_WHITE:
       str = "white";
+      break;
+    case FILL_BLACK:
+      str = "black";
       break;
     case FILL_BACKGROUND:
       str = "background";

src/app/DefaultParamsDialog.cpp
@@ -43,6 +43,7 @@ DefaultParamsDialog::DefaultParamsDialog(QWidget* parent)
 
   fillingColorBox->addItem(tr("Background"), FILL_BACKGROUND);
   fillingColorBox->addItem(tr("White"), FILL_WHITE);
+  fillingColorBox->addItem(tr("Black"), FILL_BLACK);
 
   thresholdMethodBox->addItem(tr("Otsu"), OTSU);
   thresholdMethodBox->addItem(tr("Sauvola"), SAUVOLA);

src/core/filters/output/ColorCommonOptions.h
@@ -11,7 +11,7 @@ class QDomDocument;
 class QDomElement;
 
 namespace output {
-enum FillingColor { FILL_BACKGROUND, FILL_WHITE };
+enum FillingColor { FILL_BACKGROUND, FILL_WHITE, FILL_BLACK };
 
 class ColorCommonOptions {
  public:

src/core/filters/output/OptionsWidget.cpp
@@ -45,6 +45,7 @@ OptionsWidget::OptionsWidget(std::shared_ptr<Settings> settings, const PageSelec
 
   fillingColorBox->addItem(tr("Background"), FILL_BACKGROUND);
   fillingColorBox->addItem(tr("White"), FILL_WHITE);
+  fillingColorBox->addItem(tr("Black"), FILL_BLACK);
 
   QPointer<BinarizationOptionsWidget> otsuBinarizationOptionsWidget = new OtsuBinarizationOptionsWidget(m_settings);
   QPointer<BinarizationOptionsWidget> sauvolaBinarizationOptionsWidget

src/core/filters/output/OutputGenerator.cpp
@@ -1427,10 +1427,19 @@ std::unique_ptr<OutputImage> OutputGenerator::Processor::processWithoutDewarping
   }
 
   if (m_renderParams.needBinarization() && !m_renderParams.originalBackground()) {
-    m_outsideBackgroundColor = Qt::white;
+    switch(m_colorParams.colorCommonOptions().getFillingColor()) {
+      case FILL_BLACK:
+        m_outsideBackgroundColor = Qt::black;
+        break;
+      default:
+        m_outsideBackgroundColor = Qt::white;
+    }
   } else if (m_colorParams.colorCommonOptions().getFillingColor() == FILL_WHITE) {
     m_outsideBackgroundColor = m_blackOnWhite ? Qt::white : Qt::black;
+  } else if (m_colorParams.colorCommonOptions().getFillingColor() == FILL_BLACK) {
+    m_outsideBackgroundColor = m_blackOnWhite ? Qt::black : Qt::white;
   }
+
   fillMarginsInPlace(maybeNormalized, m_contentAreaInWorkingCs, m_outsideBackgroundColor);
   dst.fill(m_outsideBackgroundColor);
 

commit 692ffd8d2f90ae81dacf6d3215ba7c2f7db2883a

Fix for #87af484 which broke the build

src/core/filters/output/ColorCommonOptions.cpp
@@ -37,13 +37,12 @@ bool ColorCommonOptions::operator!=(const ColorCommonOptions& other) const {
 }
 
 FillingColor ColorCommonOptions::parseFillingColor(const QString& str) {
-  switch(str) {
-	case "white":
-      return FILL_WHITE;
-    case "black":
-      return FILL_BLACK;
-    default:
-      return FILL_BACKGROUND;
+  if (str == "white") {
+    return FILL_WHITE;
+  } else if (str == "black") {
+    return FILL_BLACK;
+  } else {
+    return FILL_BACKGROUND;
   }
 }
 
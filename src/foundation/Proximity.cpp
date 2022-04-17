6bc208f83c644c8085ad279dfb6b5de99dd53260
kunzjacq <kunzjacq@users.noreply.github.com>
2022-04-17 14:43:09 +0200

    Add missing include for Qt 6.3.0 compatibility

--- src/foundation/Proximity.cpp
+++ src/foundation/Proximity.cpp
@@ -6,6 +6,8 @@
 #include <QLineF>
 #include <QPointF>
 
+#include <algorithm>
+
 Proximity::Proximity(const QPointF& p1, const QPointF& p2) {
   const double dx = p1.x() - p2.x();
   const double dy = p1.y() - p2.y();
